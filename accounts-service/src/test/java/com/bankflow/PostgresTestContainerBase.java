d13ebe4e74057e256c33d5ba013fa9a8b46d64cf
bsasieta <bsasieta@uoc.edu>
2026-08-15 10:16:45 +0200

    corregidas las vulnerabilidades de seguridad

---
+++ accounts-service/src/test/java/com/bankflow/PostgresTestContainerBase.java
@@ -0,0 +1,26 @@
+package com.bankflow;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public abstract class PostgresTestContainerBase {
+
+    @Container
+    public static PostgreSQLContainer<?> postgresContainer =
+            new PostgreSQLContainer<>(DockerImageName.parse("postgres:14"))
+                    .withDatabaseName("bankflow")
+                    .withUsername("bf_user")
+                    .withPassword("bf_pass");
+
+    @DynamicPropertySource
+    static void configureDatabase(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+}
--- accounts-service/src/test/java/com/bankflow/accounts/repository/AccountRepositoryIT.java
+++ accounts-service/src/test/java/com/bankflow/accounts/repository/AccountRepositoryIT.java
@@ -1,40 +1,21 @@
 package com.bankflow.accounts.repository;
 
+import com.bankflow.PostgresTestContainerBase;
 import com.bankflow.accounts.entity.Account;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Testcontainers
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @WithMockUser
-class AccountRepositoryIT {
-
-    @Container
-    public static PostgreSQLContainer<?> postgresContainer =
-            new PostgreSQLContainer<>("postgres:14")
-                    .withDatabaseName("bankflow")
-                    .withUsername("bf_user")
-                    .withPassword("bf_pass");
-
-    @DynamicPropertySource
-    static void configureDatabase(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", postgresContainer::getUsername);
-        registry.add("spring.datasource.password", postgresContainer::getPassword);
-    }
+class AccountRepositoryIT extends PostgresTestContainerBase {
 
     @Autowired
     private AccountRepository accountRepository;
